0b37d87a0007c63367c411681b88c31290fe5948
Michael Uchenna Akpa <ucheyakpa@gmail.com>
2023-06-07 01:10:58 +0100

    Added piano

---
+++ piano/main.js
@@ -0,0 +1,12 @@
+const pianoKeys = document.querySelectorAll(".key");
+
+function playSound(newUrl) {
+  new Audio(newUrl).play();
+}
+
+pianoKeys.forEach((pianoKey, i) => {
+  const number = i < 9 ? "0" + (i + 1) : i + 1;
+  const newUrl = "./key-sound/key" + number + ".mp3";
+  console.log(newUrl);
+  pianoKey.addEventListener("click", () => playSound(newUrl));
+});
